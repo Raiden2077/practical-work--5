601c600f664e30a0c25c76734dacc7de467f778c
Danil Burdin <burdin20075@gmail.com>
2026-09-17 23:35:15 +0300

    Убрана поддержка Linux2

--- PR5/PR5/PR5.cpp
+++ PR5/PR5/PR5.cpp
@@ -234,6 +234,9 @@ enum class GameState {
 
 int main()
 {
+	system("cls"); //НЕТ ПОДДЕРЖКИ ЛИНУКС!
+
+
 	srand(static_cast<unsigned int>(std::time(nullptr)));
 	sf::RenderWindow window(sf::VideoMode(1280, 720), "Snowman adventure");
 	window.setFramerateLimit(120);
